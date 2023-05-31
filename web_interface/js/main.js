b70175a7cb35c669138439429c9280f83126ceaf
iwas108 <64204327+iwas108@users.noreply.github.com>
2023-05-31 19:12:54 +0800

    WIP

--- web_interface/js/main.js
+++ web_interface/js/main.js
@@ -105,7 +105,7 @@ $( document ).ready(function() {
         else if(message.destinationName == "undiknas/ti/aktuator/kipas/"+groupId+"/ambang-batas"){
             ambangBatas = parseFloat(message.payloadString);
             $('#rangeSuhu').val(ambangBatas);
-            $("#ambangBatas").html($(ambangBatas));
+            $("#ambangBatas").html(ambangBatas);
         }
     }
 
